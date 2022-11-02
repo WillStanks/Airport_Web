8236545e87564a4eb9e80ba1a0af7a0df7bcf2a7
WillStanks <william4800@hotmail.com>
2022-11-02 15:10:01 -0400

    Debut section 7

--- airport_v0_0_0/templates/Cities/add.php
+++ airport_v0_0_0/templates/Cities/add.php
@@ -7,6 +7,14 @@
  */
 ?>
 <?php
+
+echo $this->Html->script(
+    [
+        'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.6/angular.js'
+    ],
+    ['block' => 'scriptLibraries']
+);
+
 $urlToLinkedListFilter = $this->Url->build([
     "controller" => "ProvincesStates",
     "action" => "getByCountry",
@@ -24,13 +32,25 @@ echo $this->Html->script('Cities/add_edit', ['block' => 'scriptBottom']);
         </div>
     </aside>
     <div class="column-responsive column-80">
-        <div class="cities form content">
+        <div class="cities form content" ng-app="linkedlists" ng-controller="ProvincesStatesController">
             <?= $this->Form->create($city) ?>
             <fieldset>
                 <legend><?= __('Add City') ?></legend>
+                <div>
+                    <?= __('Countries') ?> :
+                    <select name="country_id" id="country-id" ng-model="country" ng-options="country.nazev for country in Countries track by country.id">
+                        <option value=''>Select</option>
+                    </select>
+                </div>
+                <div>
+                    <?= __('Provinces') . ' ' . '(States)' ?> :
+                    <select name="provincesStates_id" id="provincesStates-id" ng-disabled="!country" ng-model="provincesStates" ng-options="provincesStates.province_states for provincesStates in country.provincesStates track by provincesStates.id">
+                        <option value=''>Select</option>
+                    </select>
+                </div>
                 <?php
-                echo $this->Form->control('country_id', ['options', $countries]);
-                echo $this->Form->control('province_id', ['options' => [__('Please select a Country first')]]);
+                // echo $this->Form->control('country_id', ['options', $countries]);
+                //echo $this->Form->control('province_id', ['options' => [__('Please select a Country first')]]);
                 echo $this->Form->control('city');
                 ?>
             </fieldset>
